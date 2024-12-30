8b421f5ed04640a5f7c717f0e91c34bddcc45366
Justis <davisjustis3@gmail.com>
2024-12-30 20:45:18 +0100

    Added functionality to the remove button with cleaner code. Added an edit button without functionallity. LOACLSTORAGE BABYYYY

--- scripts/create-new.js
+++ scripts/create-new.js
@@ -1,30 +1,40 @@
+export let todoList = [];
+todoList = localStorage.getItem("todoList") ? JSON.parse(localStorage.getItem("todoList")) : []
 
-let todoList = [];
+
 let todoItemHTML = '';
 const todoInput = document.querySelector('.js-name-todo');
 const dateDue = document.querySelector('.js-date-input');
 const commentText = document.querySelector('.js-comment-text');
 let addBtn = document.querySelector('.js-add-btn');
-let removeBtn = document.querySelectorAll('.remove-todo');
+let todoContainer = document.getElementById
+  ('todo-item-container');
+let todoContainerParent = document.querySelector('#todo-list-container-create');
+
 
 
 addBtn.addEventListener("click", function (){
-  todoList.push({
-    name: todoInput.value,
-    dueDate: dateDue.value,
-    comments: commentText.value
-  });
-    
+  pushTodo();
   addTodo();
-
   todoInput.value = '';
   dateDue.value = '';
   commentText.value = '';
   console.log(todoList);
 }
 );
+function pushTodo() {
+  todoList.push({
+    name: todoInput.value,
+    dueDate: dateDue.value,
+    comments: commentText.value
+  });
+  localStorage.setItem("todoList", JSON.stringify(todoList))
+};
+function addTodo() {
+  let displayTodos = todoList.length == 0 ? todoContainerParent.style.display = 'none' : todoContainerParent.style.display = 'flex';
+  
 
-function addTodo(){
+  console.log(todoList);
   let todoContainer = document.getElementById
     ('todo-item-container');
   todoItemHTML = '';
@@ -35,12 +45,39 @@ function addTodo(){
             <h1>${todo.name}</h1>
             <span>Due: </span><span>${todo.dueDate}</span>
             <p>Comments: </p><span>${todo.comments}</span>
-            <button class="remove-todo" onclick="todoList.splice(${index}, 1); addTodo();">Remove</button>
+            <div id="button-container">
+            <button class="remove-todo"><i class="fa-solid fa-x"></i></button>
+            <button class="edit-todo"><i class="fa-regular fa-pen-to-square"></i></button>
+            </div>
           </div>
     `;
   });
   todoContainer.innerHTML = todoItemHTML;
-  
+  activateDeleteListeners();
 };
+addTodo();
+function activateDeleteListeners() {
+  let removeBtn = document.querySelectorAll('.remove-todo');
+  removeBtn.forEach((deleteB, i) => {
+    deleteB.addEventListener("click", () => { deleteItem(i);})
+  })
+}
+
+function deleteItem(i) {
+  todoList.splice(i, 1);
+  localStorage.setItem("todoList", JSON.stringify(todoList))
+  addTodo()
+}
+function activateEditListeners() {
+  const editBtn = document.querySelectorAll('.edit-todo');
+  editBtn.forEach((editB, i) => {
+    editB.addEventListener("click", () => {
+      editButton(i);
+    })
+  })
+}
+function editButton(i) {
+  
+}
 
 
--- scripts/my-tasks.js
+++ scripts/my-tasks.js
@@ -1,11 +1,14 @@
+import { todoList } from '../scripts/create-new.js';
+
+
 console.log(document.querySelector('.js-today-date').innerHTML);
 
-// function displayTodaysDate() {
-//   let date = new Date();
-//   date = date.toString().split(" ");
-//   document.querySelector('.js-today-date').innerHTML = 10;
-//   console.log(date);
-// }
-// window.onload = function (){
-//   displayTodaysDate();
-// }
+function displayTodaysDate() {
+  let date = new Date();
+  date = date.toString().split(" ");
+  document.querySelector('.js-today-date').innerHTML = date[1] + "/" + date[2] + "/" + date[3];
+ 
+}
+window.onload = function (){
+  displayTodaysDate();
+}
