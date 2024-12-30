5fd5bde91879fa8a2e8cb9c469dc04e5c1e8575e
Justis <davisjustis3@gmail.com>
2024-12-30 17:54:48 +0100

    Separating html and js to their respective pages. Create has its own html and js pages

--- scripts/create-new.js
+++ scripts/create-new.js
@@ -42,14 +42,5 @@ function addTodo(){
   todoContainer.innerHTML = todoItemHTML;
   
 };
-console.log(document.querySelector('#todays-date-container').innerHTML);
-// function displayTodaysDate() {
-//   let date = new Date();
-//   date = date.toString().split(" ");
-//   document.querySelector('.js-today-date').innerHTML = 10;
-//   console.log(date);
-// }
-// window.onload = function (){
-//   displayTodaysDate();
-// }
+
 
---
+++ scripts/my-tasks.js
@@ -0,0 +1,11 @@
+console.log(document.querySelector('.js-today-date').innerHTML);
+
+// function displayTodaysDate() {
+//   let date = new Date();
+//   date = date.toString().split(" ");
+//   document.querySelector('.js-today-date').innerHTML = 10;
+//   console.log(date);
+// }
+// window.onload = function (){
+//   displayTodaysDate();
+// }
